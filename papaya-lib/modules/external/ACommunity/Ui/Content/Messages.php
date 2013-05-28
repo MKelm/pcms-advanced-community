21863df9166100abde5f9637e28a719e0b0c48a7
Martin Kelm <martinkelm@shrt.ws>
2013-05-28 16:11:25 +0200

    extracted collection paging to own class

--- papaya-lib/modules/external/ACommunity/Ui/Content/Messages.php
+++ papaya-lib/modules/external/ACommunity/Ui/Content/Messages.php
@@ -16,13 +16,16 @@
  * @subpackage External-ACommunity
  */
 
+// ui control collection paging
+require_once(dirname(__FILE__).'/../Control/Collection/Paging.php');
+
 /**
  * Advanced community ui content messages
  *
  * @package Papaya-Modules
  * @subpackage External-ACommunity
  */
-class ACommunityUiContentMessages extends PapayaUiControlCollection {
+class ACommunityUiContentMessages extends ACommunityUiControlCollectionPaging {
 
   /**
   * Only {@see PapayaModuleGuestbookUiContentBookEntry} objects are allowed in this list
@@ -39,101 +42,8 @@ class ACommunityUiContentMessages extends PapayaUiControlCollection {
   protected $_tagName = 'messages';
 
   /**
-  * Paging object
-  *
-  * @var PapayaUiPagingCount
-  */
-  protected $_paging = NULL;
-
-  /**
-   * Paging parameter group name
-   * @var string
-   */
-  public $pagingParameterGroup = NULL;
-
-  /**
    * Parameter name to change page
    * @var string
    */
   public $pagingParameterName = 'messages_page';
-
-  /**
-   * Items per page
-   * @var integer
-   */
-  public $pagingItemsPerPage = NULL;
-
-  /**
-   * Absolute count of items
-   * @var integer
-   */
-  public $absCount = NULL;
-
-  /**
-  * Reference object to create urls
-  * @var PapayaUiReference
-  */
-  protected $_reference = NULL;
-
-  /**
-   * Append comments output to parent element.
-   *
-   * @param PapayaXmlElement $parent
-   * @return PapayaXmlElement|NULL parent the elements where appended to,
-   *    NULL if no items are appended.
-   */
-  public function appendTo(PapayaXmlElement $parent) {
-    if (count($this->_items) > 0) {
-      $parent = $parent->appendElement(
-        $this->_tagName
-      );
-      $this->paging()->appendTo($parent);
-      foreach ($this->_items as $item) {
-        $item->appendTo($parent);
-      }
-      return $parent;
-    }
-    return NULL;
-  }
-
-  /**
-   *
-   * Paging object
-   *
-   * @param PapayaUiPagingCount $paging
-   */
-  public function paging(PapayaUiPagingCount $paging) {
-    if (isset($paging)) {
-      $this->_paging = $paging;
-    } elseif (is_null($this->_paging)) {
-      $parameter = sprintf(
-        '%s[%s]', $this->pagingParameterGroup, $this->pagingParameterName
-      );
-      $this->_paging = new PapayaUiPagingCount(
-        $parameter,
-        $this->papaya()->request->getParameter($parameter),
-        $this->absCount
-      );
-      $this->_paging->papaya($this->papaya());
-      $this->_paging->itemsPerPage = $this->pagingItemsPerPage;
-      $this->_paging->reference($this->reference());
-    }
-    return $this->_paging;
-  }
-
-  /**
-  * The basic reference object used by the subobjects to create urls.
-  *
-  * @param PapayaUiReference $reference
-  * @return PapayaUiReference
-  */
-  public function reference(PapayaUiReference $reference = NULL) {
-    if (isset($reference)) {
-      $this->_reference = $reference;
-    } elseif (is_null($this->_reference)) {
-      $this->_reference = new PapayaUiReference();
-      $this->_reference->papaya($this->papaya());
-    }
-    return $this->_reference;
-  }
 }
---
+++ papaya-lib/modules/external/ACommunity/Ui/Control/Collection/Paging.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Advanced community ui control collection with paging support
+ *
+ * @copyright 2013 by Martin Kelm
+ * @link http://idx.shrt.ws
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, version 2
+ *
+ * You can redistribute and/or modify this script under the terms of the GNU General Public
+ * License (GPL) version 2, provided that the copyright and license notes, including these
+ * lines, remain unmodified. papaya is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package Papaya-Modules
+ * @subpackage External-ACommunity
+ */
+
+/**
+ * Advanced community ui control collection with paging support
+ *
+ * @package Papaya-Modules
+ * @subpackage External-ACommunity
+ */
+class ACommunityUiControlCollectionPaging extends PapayaUiControlCollection {
+
+  /**
+  * Paging object
+  *
+  * @var PapayaUiPagingCount
+  */
+  protected $_paging = NULL;
+
+  /**
+   * Paging parameter group name
+   * @var string
+   */
+  public $pagingParameterGroup = NULL;
+
+  /**
+   * Parameter name to change page
+   * @var string
+   */
+  public $pagingParameterName = 'page';
+
+  /**
+   * Items per page
+   * @var integer
+   */
+  public $pagingItemsPerPage = NULL;
+
+  /**
+   * Absolute count of items
+   * @var integer
+   */
+  public $absCount = NULL;
+
+  /**
+  * Reference object to create urls
+  * @var PapayaUiReference
+  */
+  protected $_reference = NULL;
+
+  /**
+   * Append comments output to parent element.
+   *
+   * @param PapayaXmlElement $parent
+   * @return PapayaXmlElement|NULL parent the elements where appended to,
+   *    NULL if no items are appended.
+   */
+  public function appendTo(PapayaXmlElement $parent) {
+    if (count($this->_items) > 0) {
+      $parent = $parent->appendElement(
+        $this->_tagName
+      );
+      $this->paging()->appendTo($parent);
+      foreach ($this->_items as $item) {
+        $item->appendTo($parent);
+      }
+      return $parent;
+    }
+    return NULL;
+  }
+
+  /**
+   *
+   * Paging object
+   *
+   * @param PapayaUiPagingCount $paging
+   */
+  public function paging(PapayaUiPagingCount $paging) {
+    if (isset($paging)) {
+      $this->_paging = $paging;
+    } elseif (is_null($this->_paging)) {
+      $parameter = sprintf(
+        '%s[%s]', $this->pagingParameterGroup, $this->pagingParameterName
+      );
+      $this->_paging = new PapayaUiPagingCount(
+        $parameter,
+        $this->papaya()->request->getParameter($parameter),
+        $this->absCount
+      );
+      $this->_paging->papaya($this->papaya());
+      $this->_paging->itemsPerPage = $this->pagingItemsPerPage;
+      $this->_paging->reference($this->reference());
+    }
+    return $this->_paging;
+  }
+
+  /**
+  * The basic reference object used by the subobjects to create urls.
+  *
+  * @param PapayaUiReference $reference
+  * @return PapayaUiReference
+  */
+  public function reference(PapayaUiReference $reference = NULL) {
+    if (isset($reference)) {
+      $this->_reference = $reference;
+    } elseif (is_null($this->_reference)) {
+      $this->_reference = new PapayaUiReference();
+      $this->_reference->papaya($this->papaya());
+    }
+    return $this->_reference;
+  }
+}
